89484fcc921cb3f0978e4aff36bcbe6689efcb60
Your Name <aarshi21st@gmail.com>
2026-07-03 17:38:00 +0530

    grading

--- program_Q.c
+++ program_Q.c
@@ -1,3 +1,4 @@
+/* pass and fail
 # include<stdio.h>
 
 float main(){
@@ -10,5 +11,27 @@ float main(){
     } else {
         printf("fail");
     }
+    return 0;
+}
+*/
+
+# include<stdio.h>
+
+float main(){
+    float mark;
+    printf("enter your mark :");
+    scanf("%d", &mark);
+    if(mark<30){
+        printf("C");
+    } 
+    else if(30<=mark<70){
+        printf("B");
+    }
+    else if(70<=mark<90){
+        printf("A");
+    } else{
+        printf("A+");
+    }
+
     return 0;
 }

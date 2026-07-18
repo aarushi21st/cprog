2b58ac8486b489d1779155772c7a924aea7e9f70
Your Name <aarshi21st@gmail.com>
2026-07-18 13:54:27 +0530

    print_all_no_except_6

--- program_Q.c
+++ program_Q.c
@@ -192,3 +192,17 @@ int main(){
 }
     */
 
+/* print all no except 6                
+# include<stdio.h>
+
+int main(){
+    int n;
+    for(n=1;n<=10;n++){
+        if(n==6){
+            continue;
+        }
+    printf("%d \n", n);
+    }
+    return 0;
+}
+    */

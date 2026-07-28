00a3514a0ba90bcae04f5e038fea51c384d94615
Your Name <aarshi21st@gmail.com>
2026-07-28 20:30:21 +0530

    indian==namaste

--- function.c
+++ function.c
@@ -18,4 +18,36 @@ void bye(){
     printf("bye");
 }
     */
-   
+/* indian == namaste
+    #include<stdio.h>
+    #include<string.h>
+
+    void namaste();
+    void bojour();
+
+    int main(){
+        char nat[100];
+        printf("enter nationality :");
+        scanf("%s", &nat);
+
+        if (strcmp(nat,"indian")==0){
+            namaste();
+        }
+        else if (strcmp(nat,"french")==0){
+            bojour();
+        } 
+        else{ 
+            printf("error");
+        }
+        return 0;
+    }
+
+    void namaste(){
+        printf("namaste");
+    }
+    void bojour(){
+        printf("bonjour");
+    }
+        */
+
+    
